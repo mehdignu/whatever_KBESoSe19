deadcee58288035ed7c7c7f187ccb558354c8edb
s0557318 <s0557318@htw-berlin.de>
2019-06-24 12:39:12 +0200

    tests without jerseyTest

--- songsWSmitSonglists/src/test/java/de/berlin/htw/ai/kbe/storage/DBSongDAOTest.java
+++ songsWSmitSonglists/src/test/java/de/berlin/htw/ai/kbe/storage/DBSongDAOTest.java
@@ -1,28 +1,13 @@
 package de.berlin.htw.ai.kbe.storage;
 
 import de.berlin.htw.ai.kbe.entities.Song;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.test.JerseyTest;
-import org.glassfish.jersey.test.TestProperties;
-import org.hibernate.Session;
-import org.hibernate.jdbc.Work;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
-
-
-import org.h2.tools.RunScript;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -30,67 +15,17 @@ import static org.junit.Assert.assertNotNull;
  *  test is SUCCESSFULL
  */
 
-public class DBSongDAOTest extends JerseyTest {
+public class DBSongDAOTest {
 
-    private static EntityManagerFactory emf;
-    private static EntityManager em;
+    private static EntityManagerFactory emf = Persistence
+            .createEntityManagerFactory("song-test");
 
     private static Song songTest;
-
-    private static DBSongsDAO dbSongsDAO;
-
-    @Override
-    protected Application configure() {
-        enable(TestProperties.LOG_TRAFFIC);
-        enable(TestProperties.DUMP_ENTITY);
-        return new ResourceConfig(DBSongsDAO.class);
-    }
-
-
-    @BeforeClass
-    public static void init() {
-
-        emf = Persistence.createEntityManagerFactory("song-test");
-        em = emf.createEntityManager();
-
-        dbSongsDAO = new DBSongsDAO(emf);
-        dbSongsDAO.em = em;
-
-        // new dataset for every test case
-        Session session = em.unwrap(Session.class);
-        session.doWork(new Work() {
-            @Override
-            public void execute(Connection connection) throws SQLException {
-                try {
-                    File script = new File(getClass().getResource("META-INF/sql/data_set.sql").getFile());
-                    RunScript.execute(connection, new FileReader(script));
-                } catch (FileNotFoundException e) {
-                    throw new RuntimeException("could not initialize with script");
-                }
-            }
-        });
-
-        //songTestObjekt mit Werte init.
-        songTest = new Song();
-
-        songTest.setAlbum("Bloom");
-        songTest.setArtist("Camila Cabello, Machine Gun Kelly");
-        songTest.setReleased(2017);
-        songTest.setTitle("Bad Things");
-
-    }
-
-    @After
-    public void clearAfter() {
-        if (em.isOpen()) {
-            em.clear();
-            em.close();
-        }
-    }
 
 
     @Test
     public void testFindSongWhenValidIdShouldReturnAccepted() {
+        DBSongsDAO dbSongsDAO = new DBSongsDAO(emf);
         Response song5Response = dbSongsDAO.getSingleRowRecord(5);
         System.out.println(song5Response);
         Assert.assertEquals(song5Response.getStatus(), 200);
@@ -98,6 +33,7 @@ public class DBSongDAOTest extends JerseyTest {
 
     @Test
     public void testFindSongWhenUnvalidIdShouldReturnNotFound() {
+        DBSongsDAO dbSongsDAO = new DBSongsDAO(emf);
         Response song5Response = dbSongsDAO.getSingleRowRecord(50);
         System.out.println(song5Response);
         Assert.assertEquals(song5Response.getStatus(), 404);
@@ -105,6 +41,7 @@ public class DBSongDAOTest extends JerseyTest {
 
     @Test
     public void testFindAllSongShouldReturnNotNull() {
+        DBSongsDAO dbSongsDAO = new DBSongsDAO(emf);
         List<Song> songList = dbSongsDAO.getAllRecords();
         for(Song song:songList) {
             System.out.println(song);
@@ -114,6 +51,7 @@ public class DBSongDAOTest extends JerseyTest {
 
     @Test
     public void testPutSongWhenSameIdShouldReturnNoContent() {
+        DBSongsDAO dbSongsDAO = new DBSongsDAO(emf);
         songTest.setId(5);
         Response response = dbSongsDAO.updateRecord(5, songTest);
         Assert.assertEquals(Response.Status.NO_CONTENT, response.getStatusInfo());
@@ -121,6 +59,7 @@ public class DBSongDAOTest extends JerseyTest {
 
     @Test
     public void testPutSongWhenNotSameIdShouldReturnBadRequest() {
+        DBSongsDAO dbSongsDAO = new DBSongsDAO(emf);
         songTest.setId(10);
         Response response = dbSongsDAO.updateRecord(5, songTest);
         Assert.assertEquals(Response.Status.BAD_REQUEST, response.getStatusInfo());
@@ -128,10 +67,13 @@ public class DBSongDAOTest extends JerseyTest {
 
     @Test
     public void testPutSongWhenNonExistanceIdShouldReturnBadRequest() {
+        DBSongsDAO dbSongsDAO = new DBSongsDAO(emf);
         songTest.setId(5);
         Response response = dbSongsDAO.updateRecord(200, songTest);
         Assert.assertEquals(Response.Status.BAD_REQUEST, response.getStatusInfo());
     }
+
+
 }
 
 
--- songsWSmitSonglists/src/test/resources/META-INF/sql/data_set.sql
+++ songsWSmitSonglists/src/test/resources/META-INF/sql/data_set.sql
@@ -1,12 +1,77 @@
+DROP TABLE IF EXISTS list_song;
+DROP TABLE IF EXISTS songlists;
+DROP TABLE IF EXISTS songs;
+DROP TABLE IF EXISTS users;
+
+--
+-- Table structure for table `songs`
+--
+CREATE TABLE songs (
+                       id int NOT NULL,
+                       album varchar(255) DEFAULT NULL,
+                       artist varchar(255) DEFAULT NULL,
+                       released int,
+                       title varchar(255) DEFAULT NULL,
+                       PRIMARY KEY (id)
+);
+
+--
+-- Table structure for table `users`
+--
+CREATE TABLE users (
+                       userId varchar(255) NOT NULL,
+                       firstName varchar(255) DEFAULT NULL,
+                       lastName varchar(255) DEFAULT NULL,
+                       password varchar(255) DEFAULT NULL,
+                       PRIMARY KEY (userId)
+);
+
+--
+-- Table structure for table `songlists`
+--
+CREATE TABLE songlists (
+                           id int NOT NULL,
+                           private bit(1) NOT NULL,
+                           name varchar(255) DEFAULT NULL,
+                           owner_userId varchar(255) DEFAULT NULL,
+                           PRIMARY KEY (id),
+                           FOREIGN KEY (owner_userId) REFERENCES users (userId)
+);
+
+--
+-- Table structure for table `list_song`
+--
+CREATE TABLE list_song (
+                           list_id int NOT NULL,
+                           song_id int NOT NULL,
+                           PRIMARY KEY (list_id, song_id),
+                           FOREIGN KEY (list_id) REFERENCES songlists(id),
+                           FOREIGN KEY (song_id) REFERENCES songs (id)
+);
 --
 -- Dumping data for table `list_song`
 --
-INSERT INTO list_song VALUES (1,1),(1,2);
+INSERT INTO list_song VALUES (2, 4),
+                             (2, 5),
+                             (3, 4),
+                             (3, 5),
+                             (4, 1),
+                             (4, 2),
+                             (7, 1),
+                             (7, 2);
 
 --
 -- Dumping data for table `songlists`
 --
-INSERT INTO songlists VALUES (1, TRUE,'ElenasPrivate','mmuster');
+INSERT INTO songlists VALUES (2, b'0', 'MmusterPublic', 'mmuster'),
+                             (3, b'0', 'ElenaPublic', 'eschuler'),
+                             (4, b'1', 'EschulerPrivate', 'eschuler'),
+                             (7, b'1', 'MmusterPrivate', 'mmuster');
+
+--
+-- Dumping data for table `users`
+--
+INSERT INTO users VALUES ('mmuster','Maxime','Muster','passwd123'),('eschuler','Elena','Schuler','passwd123');
 
 --
 -- Dumping data for table `songs`
@@ -22,8 +87,3 @@ INSERT INTO songs (id, album, artist, released, title) VALUES
 (8, 'Thank You', 'Meghan Trainor', 2016, 'No'),
 (9, 'Glory', 'Britney Spears', 2016, 'Private Show'),
 (10, 'Lukas Graham (Blue Album)', 'Lukas Graham', 2015, '7 Years');
-
---
--- Dumping data for table `users`
---
-INSERT INTO users VALUES ('mmuster','Maxime','Muster','passwd123'),('eschuler','Elena','Schuler','passwd123');
